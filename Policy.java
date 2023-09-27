152a4b47a3c422944740f2866305861f344bab1e
rgalai <144728846+rgalai@users.noreply.github.com>
2023-09-27 18:33:19 -0400

    Update Policy.java

--- Policy.java
+++ Policy.java
@@ -1,23 +1,56 @@
+/**
+ * The Policy class represents an insurance policy with various attributes and methods.
+ */
 public class Policy {
 
     // Attributes
+
+    /** The policy number of the insurance policy. */
     private String policyNumber;
+
+    /** The name of the insurance provider. */
     private String providerName;
+
+    /** The first name of the policyholder. */
     private String policyholderFirstName;
+
+    /** The last name of the policyholder. */
     private String policyholderLastName;
+
+    /** The age of the policyholder. */
     private int policyholderAge;
+
+    /** The smoking status of the policyholder. */
     private String policyholderSmokingStatus;
+
+    /** The height of the policyholder in inches. */
     private double policyholderHeightInInches;
+
+    /** The weight of the policyholder in pounds. */
     private double policyholderWeightInPounds;
 
     // Constants for pricing
+
+    /** The base fee for the insurance policy. */
     private static final double BASE_FEE = 600.0;
+
+    /** The additional fee per year of age for the policyholder. */
     private static final double AGE_FEE = 75.0;
+
+    /** The additional fee for smokers. */
     private static final double SMOKER_FEE = 100.0;
+
+    /** The BMI threshold for an additional fee. */
     private static final double BMI_THRESHOLD = 35.0;
+
+    /** The fee per BMI unit above the threshold. */
     private static final double BMI_FEE_PER_UNIT = 20.0;
 
-    // Constructor with default values
+    // Constructors
+
+    /**
+     * Constructs a new Policy object with default values for all attributes.
+     */
     public Policy() {
         // Initialize all fields with default values
         policyNumber = "";
@@ -30,7 +63,18 @@ public class Policy {
         policyholderWeightInPounds = 0.0;
     }
 
-    // Constructor with arguments to fully initialize the Policy object
+    /**
+     * Constructs a new Policy object with the provided values for all attributes.
+     *
+     * @param policyNumber             The policy number.
+     * @param providerName            The name of the insurance provider.
+     * @param policyholderFirstName   The first name of the policyholder.
+     * @param policyholderLastName    The last name of the policyholder.
+     * @param policyholderAge         The age of the policyholder.
+     * @param policyholderSmokingStatus The smoking status of the policyholder.
+     * @param policyholderHeightInInches The height of the policyholder in inches.
+     * @param policyholderWeightInPounds The weight of the policyholder in pounds.
+     */
     public Policy(String policyNumber, String providerName, String policyholderFirstName,
                   String policyholderLastName, int policyholderAge, String policyholderSmokingStatus,
                   double policyholderHeightInInches, double policyholderWeightInPounds) {
@@ -46,14 +90,23 @@ public class Policy {
     }
 
     // Getters and Setters for all fields
-    // ...
 
-    // Calculate and return the BMI of the policyholder
+    // Methods
+
+    /**
+     * Calculates and returns the Body Mass Index (BMI) of the policyholder.
+     *
+     * @return The BMI of the policyholder.
+     */
     public double calculateBMI() {
         return (policyholderWeightInPounds * 703) / (policyholderHeightInInches * policyholderHeightInInches);
     }
 
-    // Calculate and return the price of the insurance policy
+    /**
+     * Calculates and returns the price of the insurance policy based on various factors.
+     *
+     * @return The price of the insurance policy.
+     */
     public double calculateInsurancePrice() {
         double price = BASE_FEE;
 
@@ -76,6 +129,11 @@ public class Policy {
         return price;
     }
 
+    /**
+     * Returns a string representation of the Policy object.
+     *
+     * @return A string representation of the Policy object.
+     */
     @Override
     public String toString() {
         // Provide a string representation of the Policy object
